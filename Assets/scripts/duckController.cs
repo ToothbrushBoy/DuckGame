fd4af1886b604d7f59d444ea1903dc9cff8bc366
VonBartlesby <53619424+VonBartlesby@users.noreply.github.com>
2023-07-07 20:20:49 +0100

    Finish controller and fix bullet trail

--- Assets/scripts/duckController.cs
+++ Assets/scripts/duckController.cs
@@ -6,7 +6,9 @@ using UnityEngine.InputSystem;
 public class duckController : MonoBehaviour
 {
     public Controls _controls;
-
+    public float speedCap;
+    public float acc;
+    public Rigidbody2D rb;
     Vector2 moveDir = Vector2.zero; // normalised vector containing wasd direction
     private InputAction move;
 
@@ -36,5 +38,11 @@ public class duckController : MonoBehaviour
     void Update()
     {
         moveDir = move.ReadValue<Vector2>();
+        rb.AddForce(moveDir * acc);
+        Debug.Log(rb.velocity.magnitude);
+        if(rb.velocity.magnitude > speedCap)
+        {
+            rb.velocity = moveDir * speedCap;
+        }
     }
 }
