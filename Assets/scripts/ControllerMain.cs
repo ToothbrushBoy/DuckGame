41f7aa2fd7f7f090f76e50e8bdad0d47a3e3feee
VonBartlesby <53619424+VonBartlesby@users.noreply.github.com>
2023-07-08 00:45:00 +0100

    Score Tracking

--- Assets/scripts/ControllerMain.cs
+++ Assets/scripts/ControllerMain.cs
@@ -1,30 +1,42 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ControllerMain : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Vector2 duckSpawn;
+    private Vector3 duckSpawn;
     public GameObject duck;
     public GameObject farmer;
+    public GameObject duckSpawnPoint;
     public Canvas UI;
     private Canvas UiInstance;
     private GameObject duckInstance;
     private int score;
+    private TextMeshProUGUI scoreText;
+    private GameObject menu;
+    private GameObject gameUI;
 
     void Start()
     {
+        duckSpawn = duckSpawnPoint.transform.position;
         UiInstance = Instantiate(UI);
-        StartButton.onClicked += test;
+        StartButton.onClicked += startGame;
         duckEvents.death += playerDied;
         duckEvents.levelComplete += levelComplete;
+        menu = UiInstance.transform.GetChild(0).gameObject;
+        gameUI = UiInstance.transform.GetChild(1).gameObject;
+        scoreText = gameUI.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>();
     }
 
     private void levelComplete()
     {
-        Debug.Log("Worked win");
+        score++;
+        scoreText.text = score + "";
+        duckInstance.transform.position = duckSpawn;
+
     }
 
     private void playerDied()
@@ -38,11 +50,11 @@ public class ControllerMain : MonoBehaviour
         
     }
 
-    public void test()
+    public void startGame()
     {
-        Debug.Log("Worked");
-        UiInstance.gameObject.SetActive(false);
+        menu.SetActive(false);
+        gameUI.SetActive(true);
         duckInstance = Instantiate(duck, duckSpawn, Quaternion.identity);
-        GameObject farmerInstance = Instantiate(farmer, duckSpawn + new Vector2(1, 0), Quaternion.identity);
+        GameObject farmerInstance = Instantiate(farmer, duckSpawn + new Vector3(2, 0, 0), Quaternion.identity);
     }
 }
