59f546c6756bb9ba85aa8abdbd79d583a75c344b
VonBartlesby <53619424+VonBartlesby@users.noreply.github.com>
2023-07-08 12:19:45 +0100

    basic spawning

--- Assets/scripts/ControllerMain.cs
+++ Assets/scripts/ControllerMain.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using System.Linq;
 using UnityEngine;
 
 public class ControllerMain : MonoBehaviour
@@ -20,6 +21,8 @@ public class ControllerMain : MonoBehaviour
     private GameObject menu;
     private GameObject gameUI;
     private ArrayList farmers;
+    public GameObject spawnParent;
+    private Transform[] farmerSpawns;
 
     void Start()
     {
@@ -32,6 +35,7 @@ public class ControllerMain : MonoBehaviour
         gameUI = UiInstance.transform.GetChild(1).gameObject;
         scoreText = gameUI.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>();
         farmers = new ArrayList();
+        getFarmerSpawnPoints();
     }
 
     private void levelComplete()
@@ -39,6 +43,8 @@ public class ControllerMain : MonoBehaviour
         score++;
         scoreText.text = score + "";
         duckInstance.transform.position = duckSpawn;
+        killFarmers();
+        spawnFarmers(score + 1);
 
     }
 
@@ -48,10 +54,9 @@ public class ControllerMain : MonoBehaviour
         menu.SetActive(true);
         gameUI.SetActive(false);
         Destroy(duckInstance);
-        foreach(GameObject farmerInstance in farmers)
-        {
-            Destroy(farmerInstance);
-        }
+        killFarmers();
+
+
     }
 
     // Update is called once per frame
@@ -60,13 +65,45 @@ public class ControllerMain : MonoBehaviour
         
     }
 
+    void getFarmerSpawnPoints()
+    {
+        farmerSpawns = spawnParent.transform.GetComponentsInChildren<Transform>();
+        Debug.Log(farmerSpawns.Length);
+    }
+
     public void startGame()
     {
         menu.SetActive(false);
         gameUI.SetActive(true);
         duckInstance = Instantiate(duck, duckSpawn, Quaternion.identity);
-        farmers.Add(Instantiate(farmer, duckSpawn + new Vector3(2, 0, 0), Quaternion.identity));
-        farmers.Add(Instantiate(farmerShotgun, duckSpawn + new Vector3(4, 0, 0), Quaternion.identity));
-        farmers.Add(Instantiate(farmer, duckSpawn + new Vector3(6, 0, 0), Quaternion.identity));
+        spawnFarmers(score+1);
+    }
+
+    private void spawnFarmers(float difficulty)
+    {
+        ArrayList used = new ArrayList();
+        int place;
+        float currentDiff = 0f;
+        while(currentDiff < difficulty && used.Count < farmerSpawns.Length-1)
+        {
+            //this is 1 start of range becuse we want to ignore the first entry in the list
+            place = UnityEngine.Random.Range(1, farmerSpawns.Length - 1);
+            while (used.Contains(place))
+            {
+                place = UnityEngine.Random.Range(1, farmerSpawns.Length);
+            }
+            var spawn = farmerSpawns[place];
+            used.Add(place);
+            farmers.Add(Instantiate(farmer, spawn.position, Quaternion.identity));
+            currentDiff += 1;
+        }
+    }
+
+    private void killFarmers()
+    {
+        foreach (GameObject farmerInstance in farmers)
+        {
+            Destroy(farmerInstance);
+        }
     }
 }
