bdefac80deb9819aa10fecd70fc20438f7993bfe
Toothbrush <morgan.mellows@virginmedia.com>
2023-07-08 23:06:28 +0100

    HELL YEAH WOOO

--- Assets/scripts/FarmerVisualsInterface.cs
+++ Assets/scripts/FarmerVisualsInterface.cs
@@ -11,14 +11,20 @@ public class FarmerVisualsInterface : MonoBehaviour
         SHOTGUN
     }
 
+    public Transform duck;
+
     public Transform muzzle;
     public GunUpdate gun;
     private Jacket jacket;
     public SkinnedMeshRenderer skinnedMeshRenderer;
 
+    public Transform aimTarget;
+    private float diff, timer = 0f;
+
     public void setAimTarget(Transform t)
     {
         gun.SetAimTarget(t);
+        aimTarget = t;
     }
 
     public Transform GetMuzzle()
@@ -35,5 +41,17 @@ public class FarmerVisualsInterface : MonoBehaviour
     private void Start()
     {
         SetJacket(Jacket.RIFLE);
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 0.25f)
+        {
+            timer = 0f;
+            if (duck != null) aimTarget.position = new Vector3(duck.position.x, duck.position.y, duck.position.z);
+        }
+        diff = Mathf.Min(Mathf.Max(aimTarget.position.x - transform.position.x, -10f), 10f);
+        transform.rotation = Quaternion.Euler(0f, diff * 7, 0f);
     }
 }
